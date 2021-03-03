4eec89ed2e2b7a67e2d80c2650aae7c441ab13f7
yepstein1 <yaakovepstein@gmail.com>
2021-03-03 12:56:25 -0500

    fixed splice

--- functions/getTrends.js
+++ functions/getTrends.js
@@ -1,20 +1,18 @@
 exports.handler =  async function (event) {
 
 
+    const googleTrends = require('google-trends-api');
+    const array = ['red hat', 'blue ball'];
 
-        const googleTrends = require('google-trends-api');
-const helper = ['red hat', 'blue ball'];
-        const array = helper.split(",");
-        googleTrends.interestOverTime({keyword: array,
-            startTime: new Date('2020-01-01'), geo: 'US'})
-            .then(function(results){
-                console.log('These results are awesome', results);
-                //helper = results;
-            })
-            .catch(function(err){
-                console.error('Oh no there was an error', err);
-            });
-        console.log(`Hi ${helper}!`)
+    googleTrends.interestOverTime({
+        keyword: array,
+        startTime: new Date('2020-01-01'), geo: 'US'
+    })
+        .then(function (results) {
+            console.log('These results are awesome', results);
 
 
+        }).catch(function (err) {
+        console.error('Oh no there was an error', err);
+    });
 }
